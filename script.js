497dc887e408daf3fcf38c50a87dd7026e5dbc81
Gustavo Vasco de Souza <titanmini305@gmail.com>
2026-08-27 23:19:25 -0300

    script.js

--- script.js
+++ script.js
@@ -1,28 +1,58 @@
 const jogos = [];
 
-
 function adicionarJogo() {
-     let nomedojogo = document.getElementById(nomeJogo).value
-     let generodojogo = document.getElementById(generoJogo).value
-     let notaJogo = document.getElementById(notaJogo).value
-};
 
+    let nomedojogo = document.getElementById("nomeJogo").value;
+    let generodojogo = document.getElementById("generoJogo").value;
+    let nota = document.getElementById("notaJogo").value;
 
-let novoJogo = {
-    nome: nome,
-    genero: genero,
-    nota: nota
-};
+    if (nomedojogo === "" || generodojogo === "" || nota === "") {
+        alert("Preencha todos os campos!");
+        return;
+    }
 
-jogos.push(novoJogo);
+    let novoJogo = {
+        nome: nomedojogo,
+        genero: generodojogo,
+        nota: nota
+    };
+
+    jogos.push(novoJogo);
+
+    mostrarJogos();
+
+    document.getElementById("nomeJogo").value = "";
+    document.getElementById("generoJogo").value = "";
+    document.getElementById("notaJogo").value = "";
+}
+
 
 function mostrarJogos() {
 
     let texto = "";
 
     for (let i = 0; i < jogos.length; i++) {
+
         texto += "<div class='jogo'>";
-        
+        texto += "<h3>" + jogos[i].nome + "</h3>";
+        texto += "<p>Gênero: " + jogos[i].genero + "</p>";
+        texto += "<p>Nota: " + jogos[i].nota + "</p>";
+        texto += "</div>";
+
     }
-    
+
+    document.getElementById("listaJogos").innerHTML = texto;
+}
+
+
+function removerJogo() {
+
+    if (jogos.length === 0) {
+        alert("Não existem jogos cadastrados!");
+        return;
+    }
+
+    jogos.pop();
+
+    mostrarJogos();
 }
